a8b21c302b2ec350ce8496f7871b26b15bc73db6
Lucius Fowler <luciusfow@gmail.com>
2021-05-04 22:24:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name){
+   let driverName = drivers.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+     return driverName
+  }
+  function fuzzyMatch(drivers, name){
+    let driverName = drivers.filter(driverName => driverName[0] === name[0])
+    return driverName
+  }
+  function matchName(drivers, person){
+    let driverMatch = drivers.filter(driverMatch => driverMatch.name === person)
+    return driverName
+ }
+  
